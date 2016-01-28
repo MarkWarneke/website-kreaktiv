169ba06dc3e746c868f9dae84cdc39cde6f6022e
mark-mit-k <warneke.mark@gmail.com>
2016-01-28 23:25:55 +0100

    adds grunt tasks, minifize all

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
             // Set to true to enable the following options…
             expand: true,
             // cwd is 'current working directory'
-            cwd: 'src/img/',
+            cwd: 'src/images/',
             src: ['**/*.png'],
             // Could also match cwd line above. i.e. project-directory/img/
             dest: 'build/img/',
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
             // Set to true to enable the following options…
             expand: true,
             // cwd is 'current working directory'
-            cwd: './src/img/',
+            cwd: './src/images/',
             src: ['**/*.jpg'],
             // Could also match cwd. i.e. project-directory/img/
             dest: 'build/img/',
@@ -49,43 +49,81 @@ module.exports = function(grunt) {
           }
         ]
       }
-     },
-sass: {
-   dist: {                            // Target
-     options: {                       // Target options
-       style: 'expanded'
-     },
-     files: [{
-       expand: true,
-       cwd: './src/css/',
-       src: ['*.sass'],
-       dest: './build/css/',
-       ext: '.css'
-     }]
-   }
-},
-
-   watch: {
-   	files: 'src/css/*.sass',
-	tasks: ['css']
-   },
-
-   browserSync: {
-    	bsFiles: {
-         src : 'build/css/*.css'
-       },
-       options: {
-	       server: {
-		       baseDir: "./"
-	       }
-	}
     },
 
-   serve: {
-    options: {
+    sass: {
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: [{
+          expand: true,
+          cwd: './src/sass/',
+          src: ['*.sass'],
+          dest: './src/css/',
+          ext: '.css'
+        }]
+      }
+    },
+
+    cssmin: {
+      dist: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>  */'
+        },
+        files: {
+          'build/<%= pkg.name %>.min.css': ['src/css/**/*.css']
+        }
+      }
+    },
+
+    htmlmin: {                                     // Task
+      parts: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'parts/',
+          src: '**/*.html',
+          dest: 'build/parts/'
+        }]
+      },
+      index: {                                       // Another target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          './index.html': './main.html'
+        }
+      }
+    },
+
+
+
+    watch: {
+      files: 'src/**/*.*',
+      tasks: ['default']
+    },
+
+    browserSync: {
+      bsFiles: {
+        src : 'build/**/*.css'
+      },
+      options: {
+        server: {
+          baseDir: "./"
+        }
+      }
+    },
+
+    serve: {
+      options: {
         port: 9000
+      }
     }
-}
 
   });
 
@@ -95,17 +133,24 @@ sass: {
   //http://blog.grayghostvisuals.com/grunt/image-optimization/
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-browser-sync');
 
+  grunt.loadNpmTasks('grunt-serve');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'sass']);
+  grunt.registerTask('default', ['uglify', 'sass', 'cssmin', 'htmlmin']);
   // imagemin task(s)
   grunt.registerTask('image', ['imagemin']);
   grunt.registerTask('imagepng', ['imagemin:png']); // only .png files
   grunt.registerTask('imagejpg', ['imagemin:jpg']);// only .jpg files
+  // css task(s)
   grunt.registerTask('validateBootstrap', ['bootlint']);
-  grunt.registerTask('css', ['sass']);
-  grunt.loadNpmTasks('grunt-serve');
+  grunt.registerTask('css', ['sass', 'cssmin']);
+
+  grunt.registerTask('html', ['htmlmin']);
+
 
 
 };
